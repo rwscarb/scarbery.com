5cc88a029300979c5868743cf66e0c5096a02eab
Ryan Scarbery <ryan.scarbery@gmail.com>
2018-04-10 11:23:21 -0700

    add decipher work history

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -11,13 +11,22 @@ export class ResumeComponent implements OnInit {
     {year: 2006, title: 'BA in Psychology, University of California Santa Barbara'}
   ];
 
-  employmentHistory: { dateStart: Date, dateEnd: Date, employer: string, content: string[] }[] = [
+  employmentHistory: { dateStart: Date, dateEnd: Date, employer: string, content: string[], accolades: string[] }[] = [
     {
       dateStart: new Date(2011, 4),
       dateEnd: new Date(2015, 7),
       employer: 'Decipher Inc',
       content: [
-        'Developed bidirectional stateful network messaging system interacting with telephone dialing hardware'
+        'Developed bidirectional stateful network messaging system interacting with telephone dialing hardware',
+        'Designed and implemented an IDE like Vim plugin for writing business DSL documents',
+        'Responsible for a number of proprietary integrations for video, survey sampling and survey markup construction',
+        'Analyzed systems for security vulnerabilities such as OWASP top threats',
+        'Helped monitor and respond to CVE\'s affecting application stack',
+        'Helped align security controls with ISO 27002:2013, PCI-DSS and HIPAA'
+      ],
+      accolades: [
+        'Employee of the Month',
+        'Innovator of the Month',
       ]
     }
   ];
