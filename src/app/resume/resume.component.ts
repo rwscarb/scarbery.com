8b14391b5e73b1582cb0150fad3e798f0e2cd0ae
Ryan Scarbery <ryan.scarbery@gmail.com>
2018-04-10 11:53:19 -0700

    add more work history and education

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -7,11 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
 
-  education: { year: number, title: string }[] = [
-    {year: 2006, title: 'BA in Psychology, University of California Santa Barbara'}
+  education: { dateStart: Date, dateEnd: Date, title: string }[] = [
+    {
+      dateStart: new Date(2004),
+      dateEnd: new Date(2006),
+      title: 'BA in Psychology, University of California Santa Barbara'
+    }
   ];
 
   employmentHistory: { dateStart: Date, dateEnd: Date, employer: string, content: string[], accolades: string[] }[] = [
+    {
+      dateStart: new Date(2017, 7),
+      dateEnd: new Date(),
+      employer: 'Socialism Incorporated',
+      content: ['Running my own Bitcoin mining company'],
+      accolades: []
+    },
+    {
+      employer: 'FocusVision',
+      dateStart: new Date(2016, 5),
+      dateEnd: new Date(2017, 7),
+      content: [
+        'Creating Ansible playbooks for acquired businesses / platforms as SRE member',
+        'Working on migrating legacy monolithic applications to distributed/redundant systems using IaaS/SaaS solutions',
+        'Developing web applications for mobile environments; Javascript, Cordova, Python, Angular.'
+      ],
+      accolades: []
+    },
+    {
+      dateStart: new Date(2015, 7),
+      dateEnd: new Date(2016, 5),
+      employer: 'Bright.md',
+      content: [
+        `Rewrote infrastructure provisioning, configuration management and deployments from convoluted shell scripts
+          to Ansible roles and playbooks`,
+        `Quickly digested codebase in order to troubleshoot architectural issues, comment on scalability and security
+          concerns as well as fix and document bugs`,
+        'Managed AWS infrastructure (IAM, VPC, EC2, S3, Route53, SQS, SES, SNS)',
+        'Audited and recorded CSRF and XSS vulnerabilities and implemented CSRF protections',
+        'Mentored coworkers on technology stack, development tools, and best practices',
+        'Worked with integrating remote EMR (electronic medical record) systems',
+        'Worked closely with client services team to respond to client issues and requests',
+        'Deployed monitoring and alerting solutions using third party tools such as DataDog, Sentry, Pingdom and ThreatStack'
+      ],
+      accolades: []
+    },
     {
       dateStart: new Date(2011, 4),
       dateEnd: new Date(2015, 7),
@@ -21,7 +61,7 @@ export class ResumeComponent implements OnInit {
         'Designed and implemented an IDE like Vim plugin for writing business DSL documents',
         'Responsible for a number of proprietary integrations for video, survey sampling and survey markup construction',
         'Analyzed systems for security vulnerabilities such as OWASP top threats',
-        'Helped monitor and respond to CVE\'s affecting application stack',
+        `Helped monitor and respond to CVE's affecting application stack`,
         'Helped align security controls with ISO 27002:2013, PCI-DSS and HIPAA'
       ],
       accolades: [
