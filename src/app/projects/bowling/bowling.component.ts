f6fb19f38802d92eeadb74ad468188ddb8573211
Ryan Scarbery <ryan.scarbery@gmail.com>
2018-04-11 11:24:05 -0700

    add initial bowling scoring

--- src/app/projects/bowling/bowling.component.ts
+++ src/app/projects/bowling/bowling.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bowling.component.css']
 })
 export class BowlingComponent implements OnInit {
+  numOfFrames = 10;
+  frames: [[number, number]];
+  currentFrame = 0;
+  attempt = 0;
+  gameOver = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.initFrames();
+  }
+
+  private initFrames() {
+    this.frames = [];
+    for (let i = 0; i < this.numOfFrames; i++) {
+      this.frames.push([0, 0]);
+    }
+  }
+
+  onScore(score: number) {
+    this.frames[this.currentFrame][this.attempt] = score;
+    this.attempt = (this.attempt +  1) % 2;
+    if (this.attempt === 0) {
+      this.currentFrame++;
+      if (this.currentFrame === this.numOfFrames) {
+        this.gameOver = true;
+      }
+    }
   }
 
 }
