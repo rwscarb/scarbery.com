57a7b9217d3530669fe69d0a1f65de7309ef16a4
Ryan Scarbery <ryan.scarbery@gmail.com>
2018-04-15 23:29:56 -0700

    taps and pipes for transforming data

--- src/app/projects/bowling/bowling.service.ts
+++ src/app/projects/bowling/bowling.service.ts
@@ -1,11 +1,11 @@
-import 'rxjs/add/operator/map';
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Frame } from "./models/frame.model";
 import { Player } from "./models/player.model";
 import { ATTEMPTS_PER_FRAME, NUM_OF_FRAMES } from "./bowling.constants";
 import { Game } from "./models/game.model";
+import { Observable } from "rxjs/Observable";
+import { map } from "rxjs/operators";
 
 
 const SERVER = 'http://127.0.0.1:8000';
@@ -14,7 +14,8 @@ const SERVER = 'http://127.0.0.1:8000';
 @Injectable()
 export class BowlingService {
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient) {
+  }
 
   initEmptyFrames(): Frame[] {
     let frames: Frame[] = [];
@@ -31,10 +32,13 @@ export class BowlingService {
     return this.http.get(SERVER + '/v1/bowling/games/' + gameID);
   }
 
-  newGame() {
-    return this.http.post(SERVER + '/v1/bowling/games', {
+  getPlayers(): Observable<Player[]> {
+    return this.http.get(SERVER + '/v1/bowling/players')
+      .pipe(map(dataArray => dataArray.map(data => new Player(data['id'], data['name']))));
+  }
 
-    });
+  newGame() {
+    return this.http.post(SERVER + '/v1/bowling/games', {});
   }
 
   score(game: Game, player: Player, frame: number, attempt: number, score: number) {
--- src/app/projects/bowling/newgame/newgame.component.ts
+++ src/app/projects/bowling/newgame/newgame.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BowlingService } from "../bowling.service";
+import { Player } from "../models/player.model";
+
 
 @Component({
   selector: 'app-newgame',
@@ -7,10 +9,13 @@ import { BowlingService } from "../bowling.service";
   styleUrls: ['./newgame.component.css']
 })
 export class NewgameComponent implements OnInit {
+  players: Player[] = [];
 
   constructor(private bowlingService: BowlingService) { }
 
   ngOnInit() {
+    this.bowlingService.getPlayers()
+      .subscribe(players => this.players = players);
   }
 
   startNewGame() {
